8f4dd4c8ec0a4c05f0806edf499c5f10960eb2e1
Valentin Lamothe <valentinlamothe56@gmail.com>
2023-04-25 22:01:28 +0200

    fix updater

--- updater.js
+++ updater.js
@@ -3,7 +3,7 @@ const AutoGitUpdate = require('auto-git-update');
 const config = {
     repository: 'https://github.com/TeamNovaFR/LifeServerFramework',
     fromReleases: true,
-    tempLocation: 'lsf_temp',
+    tempLocation: `${__dirname}/lsf_temp`,
     ignoreFiles: ['config.json'],
     executeOnComplete: 'start.bat',
     exitOnComplete: true
