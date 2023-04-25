7cc746400f87d76cf0935716bb798b6ce51d612a
Valentin Lamothe <valentinlamothe56@gmail.com>
2023-04-25 19:56:56 +0200

    Create base files

---
+++ lsf/server/index.js
@@ -0,0 +1,6 @@
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const axios         = require('axios');
+const config        = require('../../config.json');
+const io            = require('socket.io');
+
